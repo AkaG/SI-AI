6000403e5f7116e4fea960142e6edc858058ffc5
Jakub Stanczak <kuba0095@gmail.com>
2017-02-08 22:14:09 +0100

    Fix algorithm, add simple eval function

--- src/main/java/put/ai/games/js_ps_player/JS_PS_Player.java
+++ src/main/java/put/ai/games/js_ps_player/JS_PS_Player.java
@@ -4,20 +4,20 @@
  */
 package put.ai.games.js_ps_player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
-import static java.lang.Math.max;
-import static java.lang.Math.min;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.lang.Math.*;
 import static java.lang.Thread.sleep;
 
 public class JS_PS_Player extends Player {
 
-    private Random random=new Random(0xdeadbeef);
+    private int MAX_EVAL = 1000;
 
     @Override
     public String getName() {
@@ -26,77 +26,140 @@ public class JS_PS_Player extends Player {
 
     @Override
     public Move nextMove(Board b) {
-        List<Node> nodes = createNodeList(b.getMovesFor(getColor()));
-        return chooseBest(nodes, b);
-    }
+        Node ret = alfaBeta(b, 12, Integer.MIN_VALUE, Integer.MAX_VALUE, this.getColor());
 
-    private Move chooseBest(List<Node> nodes, Board board){
-        for(Node n : nodes)
-            n.value = alfaBeta(board, 1, 0,0,this.getColor());
-        return maxValueNode(nodes).getMove();
-    }
-
-    private Node maxValueNode(List<Node> nodes){
-        Node toReturn = nodes.get(0);
-        for (Node n : nodes)
-            if(toReturn.value < n.value)
-                toReturn = n;
-        return toReturn;
-    }
-
-    private int alfaBeta(Board board, int depth, int alfa, int beta, Color player){
-        if(isTerminalState(board) || depth == 0)
-            return evalState(board, player);
-
-        Board clone = board.clone();
-        if(player == this.getColor()){
-            for (Move m : board.getMovesFor(player)){
-                clone.doMove(m);
-                alfa = max(alfa, alfaBeta(clone, depth-1, alfa, beta, player == Color.PLAYER1 ? Color.PLAYER2 : Color.PLAYER1));
-                if(alfa >= beta)
-                    return beta;
-                clone.undoMove(m);
-            }
-            return alfa;
-        }else {
-            for (Move m : board.getMovesFor(player)){
-                clone.doMove(m);
-                beta = min(alfa, alfaBeta(clone, depth-1, alfa, beta, player == Color.PLAYER1 ? Color.PLAYER2 : Color.PLAYER1));
-                if(alfa >= beta)
-                    return alfa;
-                clone.undoMove(m);
-            }
-            return beta;
+        if(ret.getMove() == null){
+            Random generator = new Random();
+            List<Move> moves = b.getMovesFor(this.getColor());
+            ret.setMove(moves.get(generator.nextInt(moves.size())));
         }
+
+        return ret.getMove();
     }
 
-    private int evalState(Board b, Color player){
+    private int evalState(Board board, Color currPlayer) {
         int result = 0;
 
-        List<Move> p1 = b.getMovesFor(this.getColor());
-        List<Move> p2 = this.getColor() == Color.PLAYER1 ? b.getMovesFor(Color.PLAYER2) :  b.getMovesFor(Color.PLAYER1);
+        List<Move> maxPlayer = board.getMovesFor(Color.PLAYER1);
+        List<Move> minPlayer = board.getMovesFor(Color.PLAYER2);
+
+        result += maxPlayer.size()*2;
+        result -= minPlayer.size()*2;
+
+        //max player
+        if (currPlayer == Color.PLAYER1) {
+            if (maxPlayer.size() == 0)
+                return -MAX_EVAL;
+            if (minPlayer.size() == 0)
+                return MAX_EVAL;
+
+            for (int i = 0; i < board.getSize(); i++) {
+                for (int j = 0; j < board.getSize(); j++) {
+                    if(board.getState(i,j) == Color.PLAYER1) {
+//                        result -= sqrt(abs(i - (board.getSize() - (j + 1))));
+                        result -= result += sqrt(abs(board.getSize() - (i + j + 1)));
+                    }
+                }
+            }
+
+        } else { // min player
+            if (minPlayer.size() == 0)
+                return MAX_EVAL;
+            if (maxPlayer.size() == 0)
+                return -MAX_EVAL;
+
+            for (int i = 0; i < board.getSize(); i++) {
+                for (int j = 0; j < board.getSize(); j++) {
+                    if(board.getState(i,j) == Color.PLAYER2) {
+//                        result += sqrt(abs(board.getSize() - (i + j + 1)));
+                        result += sqrt(abs(i - (board.getSize() - (j + 1))));
+                    }
+                }
+            }
+        }
 
         return result;
     }
 
-    private boolean isTerminalState(Board b){
-        if(b.getMovesFor(Color.PLAYER1).size() == 0 || b.getMovesFor(Color.PLAYER2).size() == 0)
+    /*
+        player1 - max
+        player2 - min
+    */
+    private Node alfaBeta(Board board, int depth, int alfa, int beta, Color player) {
+        if (isTerminalState(board) || depth == 0)
+            return new Node(evalState(board, player), null);
+
+        Board clone = board.clone();
+        Node toReturn = new Node();
+
+        if (player == Color.PLAYER1) {
+            toReturn.value = Integer.MIN_VALUE;
+            for (Move m : board.getMovesFor(player)) {
+                clone.doMove(m);
+
+                Node tmp = alfaBeta(clone, depth - 1, alfa, beta, Color.PLAYER2);
+                alfa = max(alfa, tmp.value);
+
+                if(tmp.value > toReturn.value || toReturn.getMove() == null){
+                    toReturn.value = tmp.value;
+                    toReturn.setMove(m);
+                }
+
+                if (alfa >= beta)
+                    break;
+                clone.undoMove(m);
+            }
+        } else {
+            toReturn.value = Integer.MAX_VALUE;
+            for (Move m : board.getMovesFor(player)) {
+                clone.doMove(m);
+                Node tmp = alfaBeta(clone, depth - 1, alfa, beta, Color.PLAYER1);
+                beta = min(beta, tmp.value);
+
+                if(tmp.value < toReturn.value || toReturn.getMove() == null){
+                    toReturn.value = tmp.value;
+                    toReturn.setMove(m);
+                }
+
+                if (alfa >= beta)
+                   break;
+                clone.undoMove(m);
+            }
+        }
+
+        return toReturn;
+    }
+
+    private boolean isTerminalState(Board b) {
+        if (b.getMovesFor(Color.PLAYER1).size() == 0 || b.getMovesFor(Color.PLAYER2).size() == 0)
             return true;
         return false;
     }
 
-    public List<Node> createNodeList(List<Move> moves){
+    public List<Node> createNodeList(List<Move> moves) {
         List<Node> nodes = new ArrayList<>();
-        for(Move m : moves)
+        for (Move m : moves)
             nodes.add(new Node(m));
         return nodes;
     }
 
     class Node {
-        private Move move;
         public int value = 0;
+        private Move move;
+
+        public Node() {
+        }
 
         public Node(Move move) {
+            this.move = move;
+        }
+
+        public Node(int value, Move move) {
+            this.value = value;
+            this.move = move;
+        }
+
+        public void setMove(Move move) {
             this.move = move;
         }
 
