5bb9d9c1e8367dd9882fc21f54869e749f3f0fc9
Jakub Stanczak <kuba0095@gmail.com>
2017-02-09 12:01:00 +0100

    Improve eval func

--- src/main/java/put/ai/games/js_ps_player/JS_PS_Player.java
+++ src/main/java/put/ai/games/js_ps_player/JS_PS_Player.java
@@ -13,11 +13,11 @@ import java.util.List;
 import java.util.Random;
 
 import static java.lang.Math.*;
-import static java.lang.Thread.sleep;
 
 public class JS_PS_Player extends Player {
 
     private int MAX_EVAL = 1000;
+    private int MOVE_COST_POW = 3;
 
     @Override
     public String getName() {
@@ -26,11 +26,11 @@ public class JS_PS_Player extends Player {
 
     @Override
     public Move nextMove(Board b) {
-        int depth = b.getSize() < 20 ? 18 - b.getSize()/2 : 15 - b.getSize() / 2;
+        int depth = b.getSize() < 12 ? 18 - b.getSize() : 12 - b.getSize() / 2;
         depth = max(depth, 3);
         Node ret = alfaBeta(b, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, this.getColor());
 
-        if(ret.getMove() == null){
+        if (ret.getMove() == null) {
             Random generator = new Random();
             List<Move> moves = b.getMovesFor(this.getColor());
             ret.setMove(moves.get(generator.nextInt(moves.size())));
@@ -39,14 +39,11 @@ public class JS_PS_Player extends Player {
         return ret.getMove();
     }
 
-    private int evalState(Board board, Color currPlayer) {
-        int result = 0;
+    private long evalState(Board board, Color currPlayer) {
+        long result = 0;
 
         List<Move> maxPlayer = board.getMovesFor(Color.PLAYER1);
         List<Move> minPlayer = board.getMovesFor(Color.PLAYER2);
-
-        result += maxPlayer.size()*2;
-        result -= minPlayer.size()*2;
 
         //max player
         if (currPlayer == Color.PLAYER1) {
@@ -55,11 +52,13 @@ public class JS_PS_Player extends Player {
             if (minPlayer.size() == 0)
                 return MAX_EVAL;
 
+            result -= pow(minPlayer.size(), MOVE_COST_POW);
+
             for (int i = 0; i < board.getSize(); i++) {
                 for (int j = 0; j < board.getSize(); j++) {
-                    if(board.getState(i,j) == Color.PLAYER1) {
+                    if (board.getState(i, j) == Color.PLAYER1) {
 //                        result -= sqrt(abs(i - (board.getSize() - (j + 1))));
-                        result -= sqrt(abs(board.getSize() - (i + j + 1)));
+                        result -= pow(abs(board.getSize() - (i + j + 2)), 2);
                     }
                 }
             }
@@ -70,11 +69,13 @@ public class JS_PS_Player extends Player {
             if (maxPlayer.size() == 0)
                 return -MAX_EVAL;
 
+            result += pow(maxPlayer.size(), MOVE_COST_POW);
+
             for (int i = 0; i < board.getSize(); i++) {
                 for (int j = 0; j < board.getSize(); j++) {
-                    if(board.getState(i,j) == Color.PLAYER2) {
+                    if (board.getState(i, j) == Color.PLAYER2) {
 //                        result += sqrt(abs(board.getSize() - (i + j + 1)));
-                        result += sqrt(abs(i - (board.getSize() - (j + 1))));
+                        result += pow(abs(i - (board.getSize() - (j + 2))), 2);
                     }
                 }
             }
@@ -87,7 +88,7 @@ public class JS_PS_Player extends Player {
         player1 - max
         player2 - min
     */
-    private Node alfaBeta(Board board, int depth, int alfa, int beta, Color player) {
+    private Node alfaBeta(Board board, int depth, long alfa, long beta, Color player) {
         if (isTerminalState(board) || depth == 0)
             return new Node(evalState(board, player), null);
 
@@ -102,7 +103,7 @@ public class JS_PS_Player extends Player {
                 Node tmp = alfaBeta(clone, depth - 1, alfa, beta, Color.PLAYER2);
                 alfa = max(alfa, tmp.value);
 
-                if(tmp.value > toReturn.value || toReturn.getMove() == null){
+                if (tmp.value > toReturn.value || toReturn.getMove() == null) {
                     toReturn.value = tmp.value;
                     toReturn.setMove(m);
                 }
@@ -118,13 +119,13 @@ public class JS_PS_Player extends Player {
                 Node tmp = alfaBeta(clone, depth - 1, alfa, beta, Color.PLAYER1);
                 beta = min(beta, tmp.value);
 
-                if(tmp.value < toReturn.value || toReturn.getMove() == null){
+                if (tmp.value < toReturn.value || toReturn.getMove() == null) {
                     toReturn.value = tmp.value;
                     toReturn.setMove(m);
                 }
 
                 if (alfa >= beta)
-                   break;
+                    break;
                 clone.undoMove(m);
             }
         }
@@ -146,7 +147,7 @@ public class JS_PS_Player extends Player {
     }
 
     class Node {
-        public int value = 0;
+        public long value = 0;
         private Move move;
 
         public Node() {
@@ -156,17 +157,17 @@ public class JS_PS_Player extends Player {
             this.move = move;
         }
 
-        public Node(int value, Move move) {
+        public Node(long value, Move move) {
             this.value = value;
-            this.move = move;
-        }
-
-        public void setMove(Move move) {
             this.move = move;
         }
 
         public Move getMove() {
             return move;
+        }
+
+        public void setMove(Move move) {
+            this.move = move;
         }
     }
 }
